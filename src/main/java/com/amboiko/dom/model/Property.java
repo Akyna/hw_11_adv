ce041dca38ae58e9167270eb2418fa0fa3f09b59
Andrii Boiko <andrii.m.boiko@gmail.com>
2022-08-31 00:55:32 +0300

    Add Dom parser

---
+++ src/main/java/com/amboiko/dom/model/Property.java
@@ -0,0 +1,23 @@
+package com.amboiko.dom.model;
+
+public class Property {
+    private String name;
+    private String value;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("\n\tProperty{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", value='").append(value).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
